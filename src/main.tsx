c373d27e389c01e11e1d31e45d2ba70ae1be2140
Mariia Ushakova <m.ushakova22@gmail.com>
2023-11-30 14:45:43 +0100

    feat: add common styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { App } from './App';
+import { GlobalStyles } from './style/Global.styled';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './style/Theme.styled';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/style/Global.styled.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+import font from "./../assets/fonts/KumbhSans-VariableFont_YOPQ,wght.ttf";
+
+export const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+@font-face {
+        font-family: 'Kumbh Sans';
+        src: local('Kumbh Sans'), local('Kumbh Sans'),
+        url(${font}) format('ttf'),
+        font-weight: 300;
+        font-style: normal;
+    }
+
+body {
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.font};
+  font-family: "Kumbh Sans";
+  line-height: 1.66;
+  font-size: 15px;
+  overflow-x: hidden;
+}
+`;
