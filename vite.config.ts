0d99ea495c379b5f454856356d56dba3d311f0e5
Mariia Ushakova <m.ushakova22@gmail.com>
2023-12-15 19:58:50 +0100

    deploy: add deploy commands

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/E-commerce-product-page/",
     plugins: [
         react(),
         svgr({
